02db2e509dc26184517c48f1e23c40176dea6161
Xavier Joliot <71592312+XavierJoliot@users.noreply.github.com>
2022-07-21 00:10:20 -0400

    feat: models added

---
+++ Entity/Group.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookingByMe_back_models
+{
+    public class Group
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Entity/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookingByMe_back_models
+{
+    public class Recipe
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public int? GroupId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        public int? Duration { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Note { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
